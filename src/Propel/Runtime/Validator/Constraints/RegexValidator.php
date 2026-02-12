386fa894e588f61a2360c59286421cd4f2cf64e3
Jacob Thomason <jacob@thomason.xxx>
2026-02-11 22:20:23 -0500

    Added missing *Validator classes

---
+++ src/Propel/Runtime/Validator/Constraints/RegexValidator.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * MIT License. This file is part of the Propel package.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Propel\Runtime\Validator\Constraints;
+
+use Symfony\Component\Validator\Constraints\RegexValidator as SymfonyRegexValidator;
+
+/**
+ * Validator for Regex constraint - delegates to Symfony's RegexValidator
+ */
+class RegexValidator extends SymfonyRegexValidator
+{
+}
